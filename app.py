90d4173d547678848a0c7200796dee02fefbba5c
Anibal G <anibalg@Anibals-MacBook-Air.local>
2025-03-04 18:42:28 -0600

    Primer commit: inicialización del repositorio

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app) ## To allow direct AJAX calls
+
+@app.route('/datos', methods=['GET'])
+def home():
+    r = requests.get("https://catfact.ninja/fact")
+
+    return r.json()
+
+if __name__ == '__main__':
+   app.run(debug = True)
